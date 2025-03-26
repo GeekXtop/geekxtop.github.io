ee29573e4647c5f53acc9b2cd8181b00b666624a
GeekXtop <103322869+GeekXtop@users.noreply.github.com>
2025-03-26 18:27:40 +0000

    Site updated: 2025-03-26 18:27:40

---
+++ js/pjax.js
@@ -0,0 +1,1 @@
+!function(){let t;document.addEventListener("DOMContentLoaded",(()=>function(){try{const n=window.Pjax||function(){};t=new n({selectors:["[data-pjax]",".pjax-reload","head title",".columns",".navbar-start",".navbar-end",".searchbox link",".searchbox script","#back-to-top","#comments link","#comments script"],cacheBust:!1})}catch(t){console.warn("PJAX error: "+t)}}()))}();
